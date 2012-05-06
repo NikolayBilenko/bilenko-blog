753e0dbab1cf8a5888b2f6709b3742b6d210bdb0
Nikolay Bilenko <nikolay.bilenko@live.ru>
2012-05-06 23:52:22 +0300

    create rails

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+group :development, :test do
+	gem 'sqlite3'
+end
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'execjs'
+gem 'therubyracer'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BilenkoBlog::Application.config.secret_token = 'f15b3149e388b8825eac761784a39f3c0b9a76689f5444d2b10cd708b163f835cc1760358ac28372255f2fced256979240605a99d4bba56e4dcb6ab9784caa6e'
